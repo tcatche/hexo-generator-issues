515dedef22270099b3b8a4ae6db3c19dcd5bb74c
songlongfei <songlongfei@360.cn>
2018-01-22 12:07:58 +0800

    fix conflict with other plugins

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 'use strict';
-
+const t = require('./dist/generator');
+console.log(typeof t)
 hexo.extend.generator.register('issue', require('./dist/generator'));
 
 // hexo.extend.filter.register('before_post_render', require('./dist/filter'));
